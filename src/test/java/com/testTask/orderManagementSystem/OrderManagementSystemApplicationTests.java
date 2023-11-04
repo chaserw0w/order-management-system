e84b8ecbf12aa879f90cbb3a4aebe1ba847e7811
Taras Kurenda <chaserw0w.ti@gmail.com>
2023-11-04 12:45:44 +0200

    initial commit

---
+++ src/test/java/com/testTask/orderManagementSystem/OrderManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testTask.orderManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
